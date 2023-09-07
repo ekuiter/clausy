775eab9c40db7a49f200f5c449823098484e3902
Elias Kuiter <info@elias-kuiter.de>
2023-09-07 14:07:47 +0200

    hashmap

--- src/main.rs
+++ src/main.rs
@@ -6,76 +6,104 @@ use Expr::*;
 // randomize clause order? (scrambler?)
 // during parsing, when the hash of a particular subformula has already been mapped to a usize (already included in the formula), reuse that usize
 
+type VarId = u32;
+type ExprId = u32;
+
 #[derive(Debug)]
 struct Formula {
-    root_expr_id: usize,
-    exprs: Vec<Expr>,
-    vars: Vec<String>,
+    root_expr_id: ExprId,
+    next_expr_id: ExprId,
+    next_var_id: VarId,
+    exprs: HashMap<ExprId, Expr>,
+    vars: HashMap<VarId, String>,
     // possibly, we need a HashMap<Expr, usize> during parsing to ensure structural sharing
 }
 
 #[derive(Debug)]
 struct CNF {
-    variables: Vec<String>, // is this sorted?
+    variables: HashMap<VarId, String>, // is this sorted?
     clauses: Vec<Vec<i32>>,
 }
 
 #[derive(Debug)]
 enum Expr {
-    Var(usize),
-    Not(usize),
-    And(Vec<usize>),
-    Or(Vec<usize>),
+    Var(VarId),
+    Not(ExprId),
+    And(Vec<ExprId>),
+    Or(Vec<ExprId>),
 }
 
 impl Formula {
     fn new() -> Formula {
         Formula {
             root_expr_id: 0,
-            exprs: vec![Var(0)],
-            vars: vec![String::new()],
+            next_expr_id: 0,
+            next_var_id: 0,
+            exprs: HashMap::new(),
+            vars: HashMap::new(),
         }
     }
 
-    fn get_children_expr_ids(&self, expr_id: usize) -> &[usize] {
-        match &self.exprs[expr_id] {
+    fn get_expr(&self, expr_id: ExprId) -> &Expr {
+        self.exprs
+            .get(&expr_id)
+            .expect(&format!("could not retrieve expr with ID {}", expr_id))
+    }
+
+    fn get_expr_mut(&mut self, expr_id: ExprId) -> &mut Expr {
+        self.exprs
+            .get_mut(&expr_id)
+            .expect(&format!("could not retrieve expr with ID {}", expr_id))
+    }
+
+    fn get_var(&self, var_id: VarId) -> &String {
+        self.vars
+            .get(&var_id)
+            .expect(&format!("could not retrieve var with ID {}", var_id))
+    }
+
+    fn get_children_expr_ids(&self, expr_id: ExprId) -> &[ExprId] {
+        match self.get_expr(expr_id) {
             Var(_) => &[],
-            Not(expr_id) => slice::from_ref(&expr_id),
+            Not(expr_id) => slice::from_ref(expr_id),
             And(expr_ids) => &expr_ids[..],
             Or(expr_ids) => &expr_ids[..],
         }
     }
 
-    fn set_root(&mut self, expr_id: usize) {
+    fn set_root(&mut self, expr_id: ExprId) {
         self.root_expr_id = expr_id;
     }
 
-    fn add_expr(&mut self, expr: Expr) -> usize {
-        let new_expr_id = self.exprs.len();
-        self.exprs.push(expr);
-        new_expr_id
+    fn add_expr(&mut self, expr: Expr) -> u32 {
+        let next_expr_id = self.next_expr_id + 1;
+        self.exprs.insert(next_expr_id, expr);
+        self.next_expr_id += 1;
+        next_expr_id
     }
 
-    fn add_child(&mut self, expr_id: usize, child: Expr) -> usize {
-        let new_expr_id = self.exprs.len();
-        match &mut self.exprs[expr_id] {
+    fn add_child(&mut self, expr_id: ExprId, child: Expr) -> u32 {
+        let next_expr_id = self.next_expr_id + 1;
+        match self.get_expr_mut(expr_id) {
             Var(_) => panic!("can not add a child to a Var expr"),
-            Not(ref mut expr_id) => *expr_id = new_expr_id,
-            And(expr_ids) => expr_ids.push(new_expr_id),
-            Or(expr_ids) => expr_ids.push(new_expr_id),
+            Not(expr_id) => *expr_id = next_expr_id,
+            And(expr_ids) => expr_ids.push(next_expr_id),
+            Or(expr_ids) => expr_ids.push(next_expr_id),
         }
-        self.exprs.insert(new_expr_id, child);
-        new_expr_id
+        self.exprs.insert(next_expr_id, child);
+        self.next_expr_id += 1;
+        next_expr_id
     }
 
-    fn add_var(&mut self, var: &str) -> usize {
-        let new_var_id = self.vars.len();
-        self.vars.push(String::from(var));
-        self.add_expr(Var(new_var_id))
+    fn add_var(&mut self, var: &str) -> u32 {
+        let next_var_id = self.next_var_id + 1;
+        self.vars.insert(next_var_id, String::from(var));
+        self.next_var_id += 1;
+        self.add_expr(Var(next_var_id))
     }
 
-    fn fmt(&self, expr_id: usize, f: &mut fmt::Formatter) {
-        let mut write = |kind: &str, expr_ids: &[usize]| {
+    fn fmt(&self, expr_id: ExprId, f: &mut fmt::Formatter) {
+        let mut write = |kind: &str, expr_ids: &[u32]| {
             write!(f, "{kind}(").ok();
             let mut i = 0;
             for expr_id in expr_ids {
@@ -87,9 +115,9 @@ impl Formula {
             }
             write!(f, ")").ok();
         };
-        match &self.exprs[expr_id] {
+        match self.get_expr(expr_id) {
             Var(var_id) => {
-                write!(f, "{}", self.vars[*var_id]).ok();
+                write!(f, "{}", self.get_var(*var_id)).ok();
             }
             Not(expr_id) => {
                 write("Not", slice::from_ref(expr_id));
@@ -103,29 +131,29 @@ impl Formula {
         }
     }
 
-    fn to_nnf(&mut self, expr_id: usize) {
-        let expr = &mut self.exprs[expr_id];
-        match expr {
-            Var(_) => todo!(),
-            Not(_) => todo!(),
-            And(ref mut child_ids) | Or(ref mut child_ids) => {
-                for child_id in child_ids {
-                    let child = &self.exprs[*child_id];
-                    if let Not(child2_id) = child {
-                        // let child2 = self.exprs.get(child2_id).unwrap();
-                        // match child2 {
-                        //     Var(_) => (),
-                        //     Not(child3_id) => {
-                        //         *child_id = *child3_id;
-                        //     }
-                        //     And(_) => todo!(),
-                        //     Or(_) => todo!(),
-                        // }
-                    }
-                }
-            }
-        }
-    }
+    // fn to_nnf(&mut self, expr_id: ExprId) {
+    //     let expr = self.exprs.get_mut(&expr_id).unwrap();
+    //     match expr {
+    //         Var(_) => todo!(),
+    //         Not(_) => todo!(),
+    //         And(ref mut child_ids) | Or(ref mut child_ids) => {
+    //             for child_id in child_ids {
+    //                 let child = self.exprs.get(child_id).unwrap();
+    //                 if let Not(child2_id) = child {
+    //                     // let child2 = self.exprs.get(child2_id).unwrap();
+    //                     // match child2 {
+    //                     //     Var(_) => (),
+    //                     //     Not(child3_id) => {
+    //                     //         *child_id = *child3_id;
+    //                     //     }
+    //                     //     And(_) => todo!(),
+    //                     //     Or(_) => todo!(),
+    //                     // }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 impl fmt::Display for Formula {
