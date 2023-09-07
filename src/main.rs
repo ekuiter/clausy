03ac337774e8d29609716f82e1e36adbd8deb723
Elias Kuiter <info@elias-kuiter.de>
2023-09-07 10:31:41 +0200

    formulas

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,151 @@
-use indextree::*;
-use std::fmt;
-use ExprKind::*;
+use std::{collections::HashMap, fmt, slice};
+use Expr::*;
 
-// this probably creates copies of identical expressions
 // tseitin formula also has a 'pointer' to another formula, to ease the actual substitution
 // // add optimizations for simplification?
 // // randomize clause order? (scrambler?)
-// struct CNF {
-//     variables: HashMap<i32, String>, // is this sorted?
-//     clauses: Vec<Vec<i32>>,
-// }
+
+type VarId = u32;
+type ExprId = u32;
 
 #[derive(Debug)]
-enum ExprKind {
-    Var(String),
-    Not,
-    And,
-    Or,
+struct Formula {
+    root_expr_id: ExprId,
+    next_expr_id: ExprId,
+    next_var_id: VarId,
+    exprs: HashMap<ExprId, Expr>,
+    vars: HashMap<VarId, String>,
 }
 
-struct Expr<'a>(&'a NodeId, &'a Arena<ExprKind>);
-
-fn node<'a>(node_id: &NodeId, arena: &'a Arena<ExprKind>) -> &'a Node<ExprKind> {
-    arena.get(*node_id).expect("failed to retrieve node")
+#[derive(Debug)]
+struct CNF {
+    variables: HashMap<VarId, String>, // is this sorted?
+    clauses: Vec<Vec<i32>>,
 }
 
-impl<'a> fmt::Display for Expr<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let node_id = self.0;
-        let arena = self.1;
-        let expr_kind = node(node_id, arena).get();
-        match expr_kind {
-            Var(name) => {
-                write!(f, "{name}")
-            }
-            _ => {
-                write!(
-                    f,
-                    "{:?}({})",
-                    expr_kind,
-                    node_id
-                        .children(arena)
-                        .map(|child_id| Expr(&child_id, arena).to_string())
-                        .collect::<Vec<_>>()
-                        .join(", ")
-                )
-            }
+#[derive(Debug)]
+enum Expr {
+    Var(VarId),
+    Not(ExprId),
+    And(Vec<ExprId>),
+    Or(Vec<ExprId>),
+}
+
+impl Formula {
+    fn new() -> Formula {
+        Formula {
+            root_expr_id: 0,
+            next_expr_id: 0,
+            next_var_id: 0,
+            exprs: HashMap::new(),
+            vars: HashMap::new(),
         }
+    }
+
+    fn expr(&self, expr_id: ExprId) -> &Expr {
+        self.exprs
+            .get(&expr_id)
+            .expect(&format!("could not retrieve expr with ID {}", expr_id))
+    }
+
+    fn expr_mut(&mut self, expr_id: ExprId) -> &mut Expr {
+        self.exprs
+            .get_mut(&expr_id)
+            .expect(&format!("could not retrieve expr with ID {}", expr_id))
+    }
+
+    fn var(&self, var_id: VarId) -> &String {
+        self.vars
+            .get(&var_id)
+            .expect(&format!("could not retrieve var with ID {}", var_id))
+    }
+
+    fn children_expr_ids(&self, expr_id: ExprId) -> &[ExprId] {
+        match self.expr(expr_id) {
+            Var(_) => &[],
+            Not(expr_id) => slice::from_ref(expr_id),
+            And(expr_ids) => &expr_ids[..],
+            Or(expr_ids) => &expr_ids[..],
+        }
+    }
+
+    fn set_root(&mut self, expr_id: ExprId) {
+        self.root_expr_id = expr_id;
+    }
+
+    fn add_expr(&mut self, expr: Expr) -> u32 {
+        let next_expr_id = self.next_expr_id + 1;
+        self.exprs.insert(next_expr_id, expr);
+        self.next_expr_id += 1;
+        next_expr_id
+    }
+
+    fn add_child(&mut self, expr_id: ExprId, child: Expr) -> u32 {
+        let next_expr_id = self.next_expr_id + 1;
+        match self.expr_mut(expr_id) {
+            Var(_) => panic!("can not add a child to a Var expr"),
+            Not(expr_id) => *expr_id = next_expr_id,
+            And(expr_ids) => expr_ids.push(next_expr_id),
+            Or(expr_ids) => expr_ids.push(next_expr_id),
+        }
+        self.exprs.insert(next_expr_id, child);
+        self.next_expr_id += 1;
+        next_expr_id
+    }
+
+    fn add_var(&mut self, var: &str) -> u32 {
+        let next_var_id = self.next_var_id + 1;
+        self.vars.insert(next_var_id, String::from(var));
+        self.next_var_id += 1;
+        self.add_expr(Var(next_var_id))
+    }
+
+    fn display(&self, expr_id: ExprId, f: &mut fmt::Formatter) {
+        let mut write = |kind: &str, expr_ids: &[u32]| {
+            write!(f, "{kind}(").ok();
+            let mut i = 0;
+            for expr_id in expr_ids {
+                if i > 0 {
+                    write!(f, ", ").ok();
+                }
+                i += 1;
+                self.display(*expr_id, f);
+            }
+            write!(f, ")").ok();
+        };
+        match self.expr(expr_id) {
+            Var(var_id) => {
+                write!(f, "{}", self.var(*var_id)).ok();
+            }
+            Not(expr_id) => {
+                write("Not", slice::from_ref(expr_id));
+            }
+            And(expr_ids) => {
+                write("And", expr_ids);
+            }
+            Or(expr_ids) => {
+                write("Or", expr_ids);
+            }
+        };
+    }
+}
+
+impl fmt::Display for Formula {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.display(self.root_expr_id, f);
+        write!(f, "")
     }
 }
 
 fn main() {
-    // use Expr::*;
-    // let expr = Or(
-    //     Box::new(And(Box::new(Var(1)), Box::new(Not(Box::new(Var(2)))))),
-    //     Box::new(And(Box::new(Var(3)), Box::new(Var(1))))
-    // );
-    // dbg!(CnfVec::from(dbg!(expr)));
-
-    let arena = &mut Arena::new();
-    let root = arena.new_node(Or);
-    let a = arena.new_node(Var(String::from("a")));
-    let b = arena.new_node(Var(String::from("b")));
-    let c = arena.new_node(Var(String::from("c")));
-    let b_or_c = arena.new_node(Or);
-    let not_b_or_c = arena.new_node(Not);
-    root.append(a, arena);
-    root.append(not_b_or_c, arena);
-    root.append(b_or_c, arena);
-    b_or_c.append(b, arena);
-    b_or_c.append(c, arena);
-    not_b_or_c.append(b_or_c, arena);
-    println!("{}", Expr(&root, arena));
+    let mut f = Formula::new();
+    let a = f.add_var("a");
+    let b = f.add_var("b");
+    let c = f.add_var("c");
+    let a_and_c = f.add_expr(And(vec![a, c]));
+    let b_or_c = f.add_expr(Or(vec![b, c]));
+    let not_b_or_c = f.add_expr(Not(b_or_c));
+    let root = f.add_expr(Or(vec![a_and_c, not_b_or_c, b_or_c]));
+    f.set_root(root);
+    println!("{f}");
 }
