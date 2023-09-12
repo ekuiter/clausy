c84b80c01bfd1b49f311b74a8e8863988946ffcb
Elias Kuiter <info@elias-kuiter.de>
2023-09-12 09:24:14 +0200

    update

---
+++ src/cnf.rs
@@ -0,0 +1,7 @@
+// use std::collections::HashMap;
+
+// #[derive(Debug)]
+// pub struct CNF {
+//     variables: HashMap<i32, String>, // is this sorted? should it be?
+//     clauses: Vec<Vec<i32>>,
+// }
