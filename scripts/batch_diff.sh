20ad3a13a3cf3de3a2ccc7ed3d1475a5d92b3c1f
Elias Kuiter <info@elias-kuiter.de>
2023-12-08 09:08:34 +0100

    fixed batch script

--- scripts/batch_diff.sh
+++ scripts/batch_diff.sh
@@ -8,6 +8,6 @@ fi
 make >/dev/null 2>&1
 f=($(find "$1" -not -empty -type f -name '*.model' | sort -V | tr '\n' ' '))
 for ((i = 0; i < ${#f[@]}-1; i++)); do
-    cmd=(bin/clausy "${f[i]}" "${f[i+1]}" 'diff csv y n n')
+    cmd=("$(dirname "$0")/../bin/clausy" "${f[i]}" "${f[i+1]}" 'diff csv y n n')
     echo "$(basename "${f[i]}" .model),$(basename "${f[i+1]}" .model),$("${cmd[@]}")"
 done
